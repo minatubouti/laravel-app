8b129336b36cee69fbf79b394eedd9beccd57279
minatubouti <pingneimeinai@gmail.com>
2023-10-12 15:53:26 +0900

    日本語化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => '名前',
+    'confirm_password' => 'パスワード確認',
+    'register' => '新規登録',
+    'failed' => 'メールアドレスが違います',
+    'throttle' => 'ログイン試行回数が多すぎます。:seconds秒後に再試行してください。',
+    'login' => 'ログイン',
+    'logout' => 'ログアウト',
+    'email' => 'Eメールアドレス',
+    'password' => 'パスワード',
+    'remember_me' => 'ログイン状態を保存する',
+    'forgot_password' => 'パスワードをお忘れですか？',
+];
